949730b190f118cb74918aafb7de23caebdfa7cc
NilArj <nnilviaa@gmail.com>
2023-12-07 16:16:59 -0600

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Nilvia Projects",
+  title: "Nilvia's Projects",
   description: "Portfolio",
 };
 
